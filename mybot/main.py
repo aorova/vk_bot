e4d88b3700a3bd67c9762741703ce0d44ca46aef
aorova <orovaad@gmail.com>
2020-05-24 02:36:19 +0300

    Added functional - get pictures

--- mybot/main.py
+++ mybot/main.py
@@ -2,14 +2,15 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 import vk_api
 from datetime import datetime
 import random
+import get_pictures
 token = "2898fea46066070eacdf907c2aee08790049809f17fa45bd6db48d9f5b1aa08c35154915c9f951bc4d7c3"
 vk_session = vk_api.VkApi(token=token) #залогинились
 
 session_api = vk_session.get_api() #получили доступ к vk_api
 longpoll = VkLongPoll(vk_session) #не пингуем все время сервер сообщений, а longpull сам возвращает статус сообщений при наличии
 
-for event in longpoll.listen(): # пока все запущено
-    if event.type == VkEventType.MESSAGE_NEW: #если новое сообщение
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
         print('Сообщение пришло в: ' + str(datetime.strftime(datetime.now(), "%H:%M:%S")))
         print('Текст сообщения: ' + str(event.text))
         print(event.user_id)
@@ -17,4 +18,11 @@ for event in longpoll.listen(): # пока все запущено
         if event.from_user and not (event.from_me):
             if response == "1":
                 vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Привет, друг!', 'random_id': 0})
+            elif response == "2":
+                vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Пока, друг!', 'random_id': 0})
+            elif response == "котики":
+                attachment = get_pictures.get(vk_session,-130670107,session_api)
+                vk_session.method('messages.send', {'user_id': event.user_id, 'message': 'Держи котиков!', 'random_id': 0, "attachment": attachment})
+
+
 
